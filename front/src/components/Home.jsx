867c7fa8bcf314c89143dc434e4ca5be75bc7fa0
max <maxence.landa@gmail.com>
2024-03-21 22:36:18 +0100

    fonts + structure

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 export default function Home() {
+
+  const [page, setPage] = useState("Welcome");
+  
   return (
-    <div>
-      
+    <div className="p-5 w-full h-screen bg-gray-200 font-sen">
+      <div className="grid grid-cols-6">
+        <p className="text-3xl font-sulphur">MAXENCE LANDA</p>
+        <button>Accueil</button>
+        <button>Projets EPSI</button>
+        <button>Projets Perso</button>
+        <button>Rapport stage</button>
+        <div></div>
+        <p className="text-xl font-sulphur">BTS SIO SLAM</p>
+        <div className="col-span-5 m-10 text-center">Titre</div>
+        <p className="">Github</p>
+        <div className="col-span-5 m-10">Content</div>
+        <p className="">Linkedin</p>
+      </div>        
     </div>
   )
 }
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,21 +5,15 @@ import bob from './assets/saturatedbob.png'
 
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom"
 
-import Layout from './components/Layout.jsx'
 import Home from './components/Home.jsx'
+
 
 const router = createBrowserRouter([
   {
-    element: <Layout/>,
-    children: [
-    {
-      path: '/',
-      element: <Home/>,
-    },
-    ],
-    
+    path: '/',
+    element: <Home/>,
     errorElement: <Link to='/'><img src={bob} className='w-full h-full'></img></Link>,
-  }
+  }, 
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
