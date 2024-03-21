0da7b68afa254774790f7ef5e7ad246f3f31b520
max <maxence.landa@gmail.com>
2024-03-21 23:11:06 +0100

    fix structure

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,22 +1,29 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
 export default function Home({ title, content }) {
 
   return (
     <div className="p-5 w-full h-screen bg-gray-200 font-sen">
-      <div className="grid grid-cols-6 gap-2">
+      <div className="grid grid-cols-5 gap-2">
         <p className="text-3xl font-sulphur">MAXENCE LANDA</p>
         <Link to="/" className="text-center hover:underline">Accueil</Link>
         <Link to="/projetsepsi" className="text-center hover:underline">Projets EPSI</Link>
         <Link to="/projetsperso" className="text-center hover:underline">Projets Perso</Link>
-        <Link to="/rapports" className="text-center hover:underline">Rapport stage</Link>
-        <div></div>
-        <p className="text-xl font-sulphur">BTS SIO SLAM</p>
-        <div className="col-span-5 m-10 text-center">{title}</div>
-        <p className="">Github</p>
-        <div className="col-span-5 m-10">{ content }</div>
-        <p className="">Linkedin</p>
-      </div>        
+        <Link to="/rapports" className="text-center hover:underline">Rapport stage</Link>        
+      </div>
+      <div className="flex">
+        <div className="w-1/6">
+          <p className="text-xl font-sulphur">BTS SIO SLAM</p>
+          <p className="mt-20">Github</p>
+          <p className="mt-20">Linkedin</p>
+        </div>
+        <div className="w-5/6 mx-auto">
+          <div className="col-span-5 m-10 text-center">{title}</div>
+          <div className="col-span-5 m-10">{ content }</div>
+        </div>
+      </div>       
     </div>
+
   )
 }
